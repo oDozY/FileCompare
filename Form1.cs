b4c33e6e8075bb452f8fa7ecac32c7ca27ad9dde
oDozY <lqlwlelrltlyl11@gmail.com>
2026-04-16 15:06:14 +0900

    초기 커밋.

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+namespace FileCompare
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void splitContainer1_Panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
